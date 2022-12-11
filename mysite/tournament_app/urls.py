051ccfe3ef01374b0bfb7a6bcad482569b90b2a4
Ziomidlo <kziomek33@gmail.com>
2022-12-11 22:16:13 +0100

    implement regiser, log in and out functions with crispy.

--- mysite/tournament_app/urls.py
+++ mysite/tournament_app/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from  . import views
 
+app_name = 'tournament_app'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('tournament/<int:pk>/', views.tournament_details, name='tournament_details'),
+    path('register', views.register_request, name='register'),
+    path('login', views.login_request, name='login' ),
+    path('logout', views.logout_request, name='logout'),
 ]
--- mysite/tournament_app/views.py
+++ mysite/tournament_app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .forms import NewUserForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 
 from .models import UserProfile, Tournament
@@ -14,6 +18,43 @@ def tournament_details(request, pk):
     details = get_object_or_404(Tournament, pk = pk)
     context = {'details' : details}
     return render(request, 'tournament_app/tournament.html', context)
+
+def register_request(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Pomyślnie zarejestrowano!')
+            return redirect('tournament_app:index')
+        messages.error(request, 'Rejestracja nieudana!')
+    form = NewUserForm()
+    return render(request=request, template_name='tournament_app/register.html', context={'register_form': form})
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password = password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, 'Jesteś zalogowany jako {username}.')
+                return redirect('tournament_app:index')
+            else:
+                messages.error(request, 'błędna nazwa użytkownika lub hasło!')
+        else:
+            messages.error(request,'błędna nazwa użytkownika lub hasło!')
+    form = AuthenticationForm()
+    return render(request=request, template_name='tournament_app/login.html', context={'login_form': form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, 'Pomyślnie wylogowano.')
+    return redirect('tournament_app:index')
+
+
 
 
 
