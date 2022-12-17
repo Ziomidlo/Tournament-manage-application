ed79ba8b1b6f295d78b03e8a3482269328b76345
Ziomidlo <kziomek33@gmail.com>
2022-12-17 22:21:14 +0100

    next implementation of image files. This time alter models as well.

--- mysite/tournament_app/models.py
+++ mysite/tournament_app/models.py
@@ -3,25 +3,25 @@ from django.contrib.auth.models import AbstractUser
 from invitations.utils import get_invitation_model
 
 class User(AbstractUser):
-    logo = models.ImageField()
+    logo = models.ImageField(null=True, blank=True, upload_to="user/")
     info = models.CharField(max_length=1000)
     is_team = models.BooleanField(default=False)
 
 class Team(models.Model):
-    logo = models.ImageField()
+    logo = models.ImageField(null=True, blank=True, upload_to="team/")
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=1000)
     is_tournament = models.BooleanField(default=False)
-    players = models.ForeignKey(User, on_delete=models.CASCADE)
+    players = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     team_leader = models.BooleanField()
     date = models.DateField(auto_now=True)
 
 class Tournament(models.Model):
-    logo = models.ImageField()
+    logo = models.ImageField(null=True, blank=True, upload_to="tournament/")
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     number_of_teams = models.IntegerField()
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True)
     mvp = models.BooleanField()
     date = models.DateTimeField(auto_now=True)
 
