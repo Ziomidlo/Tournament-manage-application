878cea34d68224e8d806b18767f4d4611b66cfde
Ziomidlo <kziomek33@gmail.com>
2023-01-23 00:25:26 +0100

    finish cssing all pages.

--- mysite/tournament_app/admin.py
+++ mysite/tournament_app/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Tournament, Team, User, Invitation
 
 class TournamentAdmin(admin.ModelAdmin):
-    exclude = ['is_started', 'round_number']
+    exclude = ['is_started', 'round_number', 'is_finished', 'is_drawed']
 
 
 class UserAdmin(admin.ModelAdmin):
